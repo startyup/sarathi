1fe240e656f9666c1c468258b2278f882ba3beee
Onkar Parmar <onkar@Onkars-MacBook-Pro.local>
2024-08-10 04:58:02 +0200

    add logging filter

---
+++ src/main/java/com/startyup/sarathi/config/LoggingInterceptor.java
@@ -0,0 +1,27 @@
+package com.startyup.sarathi.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        System.out.println("Request URI: " + request.getRequestURI());
+        System.out.println("HTTP Method: " + request.getMethod());
+        System.out.println("Remote Address: " + request.getRemoteAddr());
+        System.out.println("Query String: " + request.getQueryString());
+        System.out.println("Headers: ");
+
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> 
+                    System.out.println(headerName + ": " + request.getHeader(headerName))
+                );
+
+        return true; // Continue with the request processing
+    }
+}
